621ed73478a2b076a2d8c73edbd2a6ce718f9197
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-07-05 23:45:34 +0800

    Unnecessary code removed.

--- app/src/main/java/scratchpad/ArrayManipulation.java
+++ app/src/main/java/scratchpad/ArrayManipulation.java
@@ -4,10 +4,6 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class ArrayManipulation {
-    public static void main(String[] args) {
-        // Splitting the array
-        IntStream.iterate(0, i -> i + 5).limit(10).forEach(System.out::println);
-    }
 
     public int[][] chunk(int[] array, int size) {
         return IntStream.iterate(0, i -> i + size)
