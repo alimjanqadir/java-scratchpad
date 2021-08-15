914d76e8f8465cfa935c33df0ffd22f54c7983f7
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-08-15 13:12:36 +0800

    Hamcrest library added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,9 @@ dependencies {
     // Use JUnit Jupiter API for testing.
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
 
+    // Hamcrest library for assertions.
+    testImplementation("org.hamcrest:hamcrest:2.2")
+
     // Use JUnit Jupiter Engine for testing.
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
