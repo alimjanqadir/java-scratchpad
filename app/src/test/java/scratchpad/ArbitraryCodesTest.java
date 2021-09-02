6c76ba557c4029424f68e22648538ad0d60d2588
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-09-02 17:08:41 +0800

    `ArbitraryCodes` class created to save uncategorized methods(for study purposes).

---
+++ app/src/test/java/scratchpad/ArbitraryCodesTest.java
@@ -0,0 +1,14 @@
+package scratchpad;
+
+import org.junit.jupiter.api.*;
+
+public final class ArbitraryCodesTest {
+	private static ArbitraryCodes testingObject;
+	
+	@BeforeAll
+	public static void setup() {
+		testingObject = new ArbitraryCodes();
+	}
+}
+
+
