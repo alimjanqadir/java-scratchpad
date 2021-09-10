6c308b9401bbcfb7226134553aa9a6aa90ec862c
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-09-10 21:59:09 +0800

    Main method overloading experimented.

---
+++ app/src/main/java/scratchpad/MainExperiment.java
@@ -0,0 +1,16 @@
+package scratchpad;
+
+public final class MainExperiment {
+	public static void main() {
+		System.out.println("main");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("main-String args");
+	}
+	
+	public static void main(int argv) {
+		System.out.println("main-int argv");
+	}
+
+}
