2d30a5e3403953604660a16bae8e2e6ee6e9f51c
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-10-01 19:05:35 +0800

    A simple java practice.

---
+++ app/src/main/java/scratchpad/FileInputStreamPractice.java
@@ -0,0 +1,21 @@
+package scrachpad;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public final class FileInputStreamPractice {
+	public static void main(String[] args) {
+		var projectRootPath = new File("../../../../../object.data");
+		try {
+			var fileInputStream = new FileInputStream(projectRootPath);
+			while(fileInputStream.available() > 0) {
+				System.out.print((char) fileInputStream.read());
+			}
+			fileInputStream.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
