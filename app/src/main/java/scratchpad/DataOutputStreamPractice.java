f7e28c70bac1b485f27238bd8f0e15bd941eeb31
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-10-02 12:54:24 +0800

    DataOutputStream practice.

---
+++ app/src/main/java/scratchpad/DataOutputStreamPractice.java
@@ -0,0 +1,23 @@
+package scratchpad;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public final class DataOutputStreamPractice {
+	public static void main(String[] args) {
+		var integer = 2147483647;
+		var projectRootPath = "../../../../../";
+		var outputFile =  new File(projectRootPath + "integer.data");
+		try {
+			var fileOutputStreamInstance = new FileOutputStream(outputFile);
+			var dataOutputStreamInstance = new DataOutputStream(fileOutputStreamInstance);
+			dataOutputStreamInstance.writeInt(integer);
+			dataOutputStreamInstance.flush();
+			dataOutputStreamInstance.close();
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+}
