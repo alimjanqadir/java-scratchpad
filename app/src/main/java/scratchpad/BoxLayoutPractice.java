b22b8b793cffdcac931439fe9ce521a9d9acd1ba
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-10-09 13:09:24 +0800

    Swing toolkit BoxLayout practice.

---
+++ app/src/main/java/scratchpad/BoxLayoutPractice.java
@@ -0,0 +1,48 @@
+package scratchpad;
+
+import java.awt.Container;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+
+public final class BoxLayoutPractice {
+	public static void main(String[] args) {
+		var boxLayoutPractice = new BoxLayoutPractice();
+		boxLayoutPractice.render();
+	
+	}
+
+	void render(){
+		var frame = new JFrame("BoxLayout Practice");
+		var contentPane = frame.getContentPane();
+
+		var verticalPanel = new JPanel();
+		var horizontalPanel = new JPanel();
+
+		verticalPanel.setLayout(new BoxLayout(verticalPanel, BoxLayout.Y_AXIS));
+		horizontalPanel.setLayout(new BoxLayout(horizontalPanel, BoxLayout.X_AXIS));
+
+		verticalPanel.add(new JLabel("First"));
+		verticalPanel.add(new JLabel("Second"));
+		verticalPanel.add(new JLabel("Third"));
+		verticalPanel.add(new JLabel("Forth"));
+
+		contentPane.add(verticalPanel, BorderLayout.CENTER);
+
+		horizontalPanel.add(new JButton("OK"));
+		horizontalPanel.add(new JButton("Set"));
+		horizontalPanel.add(new JButton("Cancel"));
+				
+		contentPane.add(horizontalPanel, BorderLayout.SOUTH);
+
+		frame.setVisible(true);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
