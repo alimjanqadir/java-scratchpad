e653e70184c12722de4320b9083d3e311acfe227
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-11-14 18:45:59 +0800

    for in practice.

---
+++ app/src/main/java/scratchpad/ForInFloat.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public final class ForInFloat {
+	public static void main(String[] args) {
+		Random random = new Random();
+		float[] f = new float[10];
+		for(int i=0; i<f.length; i++)
+			f[i] = random.nextFloat();
+		for(float n : f)
+			System.out.println(n);	
+	}
+}
