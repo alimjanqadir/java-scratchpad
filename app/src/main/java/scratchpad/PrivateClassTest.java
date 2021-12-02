33050922185ade9217bf5386a2e291d01d130c24
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-12-02 20:18:00 +0800

    Private class definition reviewed.

---
+++ app/src/main/java/scratchpad/PrivateClassTest.java
@@ -0,0 +1,14 @@
+public class PrivateClassTest {
+	public static void main(String[] args) {
+		var a = new A();
+		System.out.println(a.toString());
+	}
+	private static class A{
+		public A(){}
+	}
+}
+
+//private class A{
+//	public A(){}
+//}
+
