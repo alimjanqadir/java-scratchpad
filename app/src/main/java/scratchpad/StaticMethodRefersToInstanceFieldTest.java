7d805a8e082a9cc1f77f24acf3bfb91ec638944a
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-02-12 14:33:35 +0800

    how static inner class can avoid memory leak, demonstrated.

---
+++ app/src/main/java/scratchpad/StaticMethodRefersToInstanceFieldTest.java
@@ -0,0 +1,10 @@
+public final class StaticMethodRefersToInstanceFieldTest {
+	int[] array = new int[]{3, 1, 4, 15};
+
+	public static void main(String[] args) {
+	// System.out.println(array.length);
+	// error: non-static variable array cannot be referenced from a static context
+
+
+	}
+}
