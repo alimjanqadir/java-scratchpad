47b7dec28dc2e06cae2e00311532e364a9982381
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-02-26 22:46:24 +0800

    Static member initialization tested.

---
+++ app/src/main/java/scratchpad/StaticInitialization.java
@@ -0,0 +1,23 @@
+public class StaticInitialization {
+	class InnerClassA {
+		InnerClassA() {
+			System.out.println("innerclass a");
+		}
+	}
+
+	static class InnerClassB {
+		InnerClassB() {
+			System.out.println("innerclass b");
+		}
+	}
+	
+	static InnerClassB classB = new InnerClassB(); 
+	InnerClassA aClass = new InnerClassA();
+	//StaticInitialization staticInit = new StaticInitialization();
+	public static void main(String[] args) {
+		new StaticInitialization();
+		for(String arg : args)
+			System.out.print(" " + arg);
+		System.out.print("\n");
+	}
+}
