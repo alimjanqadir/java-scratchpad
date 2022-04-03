b58b67faa9765fed26fdffd3f0da32affb995934
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-04-03 21:18:49 +0800

    Tested the order when static initializer invoked.

---
+++ app/src/main/java/scratchpad/StaticMemberInitialization.java
@@ -0,0 +1,25 @@
+public class StaticMemberInitialization {
+	public static void main(String[] args) {
+		var test = new ClassThatHasStaticMember();
+		//test.text();
+	}	
+}
+
+public class ClassThatHasStaticMember {
+	private static class InnerStaticClass {
+		static String text = text();
+		static String text() {
+			System.out.println("InnerStaticClass text");
+			return "text";
+		}
+	}
+	static {
+		System.out.println("static");
+	}
+
+	void text() {
+		System.out.println(InnerStaticClass.text);	
+	}
+}
+
+
