e6a05963b4a8e6dad26583df0108fec4b838501f
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-08-07 23:09:41 +0800

    Adds new algo.

---
+++ app/src/main/java/scratchpad/algo/MinimumBribes.java
@@ -0,0 +1,48 @@
+package scratchpad.algo;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+public class MinimumBribes {
+    static class Result {
+
+        /*
+         * Complete the 'minimumBribes' function below.
+         *
+         * The function accepts INTEGER_ARRAY q as parameter.
+         */
+
+        public static void minimumBribes(List<Integer> q) {
+            // Write your code here
+
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                Result.minimumBribes(q);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+}
